4b23b313903a2fe73351dc84a2927cc156b9795e
Cédric Eberhardt <hello+code@cedeber.fr>
2019-03-07 14:42:37 +0100

    try async

---
+++ src/async.ts
@@ -0,0 +1,3 @@
+export default function log(what) {
+    console.log(what);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
+if (document.body) {
+    import("./async").then(module => module.default("Hello, async!"));
+}
+
 console.log("Hello, sourcemaps!");
 
-ReactDOM.render(
-    <App who="Cédric" />,
-    document.querySelector("#app"),
-);
+ReactDOM.render(<App who="Cédric" />, document.querySelector("#app"));
