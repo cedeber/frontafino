87dbcaab2106efc6a95463fc856f8105c134f164
Cédric Eberhardt <hello+code@cedeber.fr>
2019-07-23 18:23:42 +0200

    Use babel register for the server

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-require("ts-node").register({
-    compilerOptions: {
-        module: "commonjs"
-    }
+require("@babel/register")({
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".mjs"],
+    presets: [
+        ["@babel/preset-env", {
+            "targets": {
+                "node": "current",
+            },
+        }],
+        "@babel/preset-typescript",
+        "@babel/preset-react",
+    ],
+    plugins: [
+        "@babel/proposal-class-properties",
+        "@babel/proposal-object-rest-spread",
+    ],
 });
-require("./src/server.tsx");
+
+require("./src/server/index.tsx");
