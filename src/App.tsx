6fba5cad796bd7a10be0ea1ac86a05ad3d3f4065
Cédric Eberhardt <hello+code@cedeber.fr>
2019-07-23 19:00:46 +0200

    import Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { hot } from "react-hot-loader";
-import React from "react";
+import React, { Suspense } from "react";
 import { Router, Link } from "@reach/router";
 
 import Home from "./pages/Home";
@@ -14,12 +14,12 @@ function App() {
                     <Link to="/">Home</Link> | <Link to="about">About</Link>
                 </nav>
             </header>
-            <React.Suspense fallback={<div>Loading app...</div>}>
+            <Suspense fallback={<div>Loading app...</div>}>
                 <Router>
                     <Home who="You" path="/" />
                     <About path="about" />
                 </Router>
-            </React.Suspense>
+            </Suspense>
         </>
     );
 }
