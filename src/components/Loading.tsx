b5cb90949d00beea35cafb237489a0d745becbc4
Cédric Eberhardt <hello+code@cedeber.fr>
2019-07-28 17:09:38 +0200

    test postcss

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,5 +2,8 @@ import React from "react";
 import { loading } from "../styles/components.css";
 
 export default function Loading() {
-    return <div className={loading}>Loading...</div>;
+    return <div className={loading}>
+        Loading
+        <span>...</span>
+    </div>;
 }
