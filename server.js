acb2752666e5a9f8cd158853e2957d8f16925bef
Cédric Eberhardt <hello+code@cedeber.fr>
2019-09-02 18:26:46 +0200

    Try heroku with rust

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require("http");
 const micro = require("micro");
 const handler = require("serve-handler");
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer(
     micro(async (request, response) => {
         return await handler(request, response, {
@@ -11,4 +13,5 @@ const server = http.createServer(
     }),
 );
 
-server.listen(process.env.PORT || 3000);
+console.log("Listening on port:", PORT);
+server.listen(PORT);
