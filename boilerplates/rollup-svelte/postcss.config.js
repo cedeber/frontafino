7dcbbcf2fca0307b942500bb6bcf4e459c760434
Cédric Eberhardt <hello+code@cedeber.fr>
2020-01-20 22:56:00 +0100

    Add cssnano

--- boilerplates/rollup-svelte/postcss.config.js
+++ boilerplates/rollup-svelte/postcss.config.js
@@ -1,15 +1,20 @@
 const production = !process.env.ROLLUP_WATCH;
-const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
-  plugins: [
-    require("postcss-import")(),
-    // require("tailwindcss"),
-    require("autoprefixer"),
-    production &&
-      purgecss({
-        content: ["./**/*.html", "./**/*.svelte"],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-      })
-  ]
+    plugins: [
+        require("postcss-import")(),
+        // require("tailwindcss"),
+        require("autoprefixer"),
+        production && require("@fullhuman/postcss-purgecss")({
+            content: ["./**/*.html", "./**/*.svelte"],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        }),
+        production && require('cssnano')({
+            preset: 'default',
+            calc: false,
+            discardComments: {
+                removeAll: true,
+            },
+        }),
+    ]
 };
