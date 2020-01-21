fc47ed484dce6a5d9c10038bb9bd9a9cf7886aee
ceberhar <cedric.eberhardt@genedata.com>
2020-01-21 09:16:02 +0100

    less config + tailwind css

--- boilerplates/rollup-svelte/postcss.config.js
+++ boilerplates/rollup-svelte/postcss.config.js
@@ -2,9 +2,7 @@ const production = !process.env.ROLLUP_WATCH;
 
 module.exports = {
     plugins: [
-        require("postcss-import")(),
         require("tailwindcss"),
-        require("autoprefixer"),
         production && require("@fullhuman/postcss-purgecss")({
             content: ["./src/**/*.svelte", "./public/**/*.html"],
             defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
@@ -12,9 +10,6 @@ module.exports = {
         production && require('cssnano')({
             preset: 'default',
             calc: false,
-            discardComments: {
-                removeAll: true,
-            },
         }),
     ]
 };
