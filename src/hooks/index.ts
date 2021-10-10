83c4a6b2bc1976659b1cc5061890f018f629e4fe
Cédric Eberhardt <hello@cedeber.fr>
2021-10-10 16:21:57 +0200

    export all hooks + export map

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useDistance } from "./useDistance";
+export { useForwardedRef } from "./useForwardedRef";
+export { useHotKeys } from "./useHotKeys";
+export { useLongLoading, useVisuallyLoading } from "./useLoading";
+export { useURLSearchParamsList } from "./useUrl";
